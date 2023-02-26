0c1bfce975037aee1a60e37ae82fb883e6e71ec0
MyatKaungKyaw <myatkaungg31@gmail.com>
2023-02-26 13:43:43 +0630

    Add Anecdote element and show data in it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link, useMatch } from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -18,9 +18,20 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote =>
+        <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
+      )}
     </ul>
   </div>
+)
+
+const Anecdote = ({ anecdote }) => (
+  <>
+    <h3>{`${anecdote.content} by ${anecdote.author}`}</h3>
+    <p>{`has ${anecdote.votes} votes`}</p>
+  </>
 )
 
 const About = () => (
@@ -123,17 +134,21 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const anecdoteMatch = useMatch('/anecdotes/:id')
+  const anecdote = anecdoteMatch ?
+    anecdotes.find(anecdote => anecdote.id === Number(anecdoteMatch.params.id))
+    : null
+
   return (
     <div>
-      <Router>
-        <h1>Software anecdotes</h1>
-        <Menu />
-        <Routes>
-          <Route path='/create_new' element={<CreateNew addNew={addNew} />}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/' element={<AnecdoteList anecdotes={anecdotes}/>}/>
-        </Routes>
-      </Router>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      <Routes>
+        <Route path='/anecdotes/:id' element={<Anecdote anecdote={anecdote}/>} />
+        <Route path='/create_new' element={<CreateNew addNew={addNew} />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
+      </Routes>
       <Footer />
     </div>
   )

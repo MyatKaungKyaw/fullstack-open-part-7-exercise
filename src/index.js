ad322bdae3f2963cf46c432c4dfefd9bbdd83c9b
MyatKaungKyaw <myatkaungg31@gmail.com>
2023-03-05 21:21:15 +0630

    add useField hook

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { useState } from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
         <App />
     </Router>)
+
+export const useField = type => {
+    const [value,setValue] = useState('')
+    const onChange = event =>{
+        setValue(event.target.value)
+    }
+
+    return{
+        type,
+        value,
+        onChange,
+    }
+}

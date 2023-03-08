605a625acfac1177290eea5455c6278039501498
Myat Kaung Kyaw <33856024+MyatKaungKyaw@users.noreply.github.com>
2023-03-08 23:32:48 +0900

    upload from work

--- country-hook/src/App.js
+++ country-hook/src/App.js
@@ -1,60 +1,73 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const useField = (type) => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState("");
 
   const onChange = (event) => {
-    setValue(event.target.value)
-  }
+    setValue(event.target.value);
+  };
 
   return {
     type,
     value,
-    onChange
-  }
-}
+    onChange,
+  };
+};
 
 const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
+  const [country, setCountry] = useState(null);
 
-  useEffect(() => {})
+  useEffect(() => {
+    const getCountry = async () => {
+      try {
+        const ctryRes = await axios.get(
+          `https://restcountries.com/v3.1/name/${name}?fullText=true`
+        );
+        setCountry({data:ctryRes.data[0]})
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getCountry();
+  }, [name]);
 
-  return country
-}
+  return country;
+};
 
 const Country = ({ country }) => {
+  console.log(country)
   if (!country) {
-    return null
+    return null;
   }
 
-  if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
-  }
+  // if (!country.found) {
+  //   return <div>not found...</div>;
+  // }
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
+      <h3>{country.data.name.official} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img
+        src={country.data.flag}
+        height="100"
+        alt={`flag of ${country.data.name}`}
+      />
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-  const nameInput = useField('text')
-  const [name, setName] = useState('')
-  const country = useCountry(name)
+  const nameInput = useField("text");
+  const [name, setName] = useState("");
+  const country = useCountry(name);
 
-  const fetch = (e) => {
-    e.preventDefault()
-    setName(nameInput.value)
-  }
+  const fetch = async (e) => {
+    e.preventDefault();
+    setName(nameInput.value);
+  };
 
   return (
     <div>
@@ -65,7 +78,7 @@ const App = () => {
 
       <Country country={country} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- country-hook/src/index.js
+++ country-hook/src/index.js
@@ -1,18 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import axios from "axios";
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
-export const useCountry = ctryName => {
-    const {country,setCountry}=useState()
-
-    useEffect(()=>{
-        try{
-            const countryRes = await axios.get(`https://restcountries.com/v3.1/name/${ctryName}?fullText=true`)
-        }catch(err){
-            console.error(err)
-        }
-    },[])
-}
+ReactDOM.render(<App />, document.getElementById("root"));

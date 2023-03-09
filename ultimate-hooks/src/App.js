4186315e1ddba351b9e486051f126c48124d086e
MyatKaungKyaw <myatkaungg31@gmail.com>
2023-03-09 21:21:03 +0630

    implement useResource

--- ultimate-hooks/src/App.js
+++ ultimate-hooks/src/App.js
@@ -18,10 +18,14 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
-
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    try{
+      await axios.post(baseUrl,resource)
+      const res = (await axios.get(baseUrl)).data
+      setResources(res)
+    }catch(err){
+      console.error(err)
+    }
   }
 
   const service = {
@@ -41,14 +45,14 @@ const App = () => {
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
 
-  const handleNoteSubmit = (event) => {
+  const handleNoteSubmit = async (event) => {
     event.preventDefault()
-    noteService.create({ content: content.value })
+    await noteService.create({ content: content.value })
   }
  
-  const handlePersonSubmit = (event) => {
+  const handlePersonSubmit = async (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    await personService.create({ name: name.value, number: number.value})
   }
 
   return (

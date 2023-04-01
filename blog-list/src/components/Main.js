fdf02f0c4f2e269c17836fc1681105f9084bbba8
Myat Kaung Kyaw <33856024+MyatKaungKyaw@users.noreply.github.com>
2023-04-01 09:05:03 +0000

    Create Main.js

---
+++ blog-list/src/components/Main.js
@@ -0,0 +1,14 @@
+import NotificationBar from "./NotificationBar"
+
+const Main = (props) => {
+    return (
+        <>
+        <NotificationBar/>
+        <div className={message !== null ? "show-notification-position" : ""}>
+          {props.children}
+        </div>
+      </>
+    )
+}
+
+export default Main

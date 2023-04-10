1f717bceceda16117aea9630799b46820807b7bb
MyatKaungKyaw <myatkaungg31@gmail.com>
2023-04-10 11:22:39 +0630

    add user list with grid

--- blog-list/src/components/UserList.js
+++ blog-list/src/components/UserList.js
@@ -1,7 +1,33 @@
+import userService from '../services/users'
+import { useQuery } from 'react-query'
+
 const UserList = () => {
+    const usersResult = useQuery('userrs', userService.getAll, {
+        refetchOnWindowFocus: false,
+    })
+
+    if (usersResult.isLoading) {
+        return (
+            <div>Loading users data</div>
+        )
+    }
+
+    const users = usersResult.data
+    console.log(users)
     return (
         <>
             <h2>Users</h2>
+            <div className="user-grid-container">
+                <div className="user-grid-item">
+                    <div className="user-blog">Blogs created</div>
+                </div>
+                {users.map((user) => (
+                    <div className="user-grid-item" key={user.id}>
+                        <div className="user-name">{user.name}</div>
+                        <div className="user-blog">{user.blogs.length}</div>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }

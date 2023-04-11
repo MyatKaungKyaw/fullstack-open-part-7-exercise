daeaa19f8e0e25622c22bc3008f889f9933c924d
MyatKaungKyaw <myatkaungg31@gmail.com>
2023-04-11 20:28:56 +0630

    show user detail without changing page

--- blog-list/src/components/UserDetail.js
+++ blog-list/src/components/UserDetail.js
@@ -2,6 +2,7 @@ const UserDetail = ({ name, blogs }) => {
     return (
         <>
             <h2>{name}</h2>
+            <h3>added blogs</h3>
             <ul>
                 {blogs.map(blog => (
                     <li key={blog.id}>{blog.title}</li>
@@ -10,3 +11,5 @@ const UserDetail = ({ name, blogs }) => {
         </>
     )
 }
+
+export default UserDetail
--- blog-list/src/components/UserList.js
+++ blog-list/src/components/UserList.js
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import userService from '../services/users'
 import { useQuery } from 'react-query'
+import UserDetail from './UserDetail'
 
 const UserList = () => {
+    const [name,setName]=useState(null)
+    const [blogs,setBlogs]=useState([])
     const usersResult = useQuery('userrs', userService.getAll, {
         refetchOnWindowFocus: false,
     })
+
+    const userDetailClick = (user) =>{
+        setName(user.name)
+        setBlogs(user.blogs)
+    }
 
     if (usersResult.isLoading) {
         return (
@@ -22,11 +31,17 @@ const UserList = () => {
                 </div>
                 {users.map((user) => (
                     <div className="user-grid-item" key={user.id}>
-                        <div className="user-name">{user.name}</div>
+                        <div 
+                        className="user-name"
+                        onClick={() => userDetailClick(user)}
+                        name={user.name}
+                        blogs={user.blogs}
+                        >{user.name}</div>
                         <div className="user-blog">{user.blogs.length}</div>
                     </div>
                 ))}
             </div>
+            {name && <UserDetail name={name} blogs={blogs}/>}
         </>
     )
 }
